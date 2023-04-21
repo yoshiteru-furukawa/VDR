37b5cd79574fe193742c7a44cfc1562aaaa6869d
yoshiteru-furukawa <yfurukawa@g.ecc.u-tokyo.ac.jp>
2023-04-21 14:49:36 +0900

    fix: delete some comments

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: express.Express = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//CORS対応
+//CORS
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*")
